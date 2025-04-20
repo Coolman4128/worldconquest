b4c998bb71ce020f83906bff17a4e5466a512748
Coolman4128 <tylordgaming@gmail.com>
2025-04-20 19:37:38 -0400

    fixed CORS

--- backend/WorldConquest/Program.cs
+++ backend/WorldConquest/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", builder =>
-        builder.WithOrigins("http://localhost:3000")
+        builder.WithOrigins("http://localhost:5000")
                .AllowAnyMethod()
                .AllowAnyHeader()
                .AllowCredentials());
