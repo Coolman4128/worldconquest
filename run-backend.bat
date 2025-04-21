890b63d3fa9c3f82e2adddb3d6ac236216a61a01
twatson-ten <twatson@tenney.com>
2025-04-21 12:36:10 -0400

    Restarting

--- run-backend.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Navigate to the backend directory
-cd backend\WorldConquest
-
-REM Run the ASP.NET Core application
-dotnet run
--- run-backend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Navigate to the backend directory
-cd backend/WorldConquest
-
-# Run the ASP.NET Core application
-dotnet run
